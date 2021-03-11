e63faeb95e1533938e10505e0076f6910891a465
johnmacha <64866022+johnmacha@users.noreply.github.com>
2021-03-11 10:53:08 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+
+
+function App() {
+
+  return (
+    
+<Game /> 
+
+  );
+}
+
+export default App;
